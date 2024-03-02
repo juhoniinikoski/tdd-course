40624f8b49d88580541013d4f5bebeaa11e0845d
Juho Niinikoski <niinikoskijuho@gmail.com>
2024-03-02 13:34:05 +0200

    modified parseDate

--- part1/src/prices_typed.ts
+++ part1/src/prices_typed.ts
@@ -1,6 +1,7 @@
 import "./polyfills";
 import express from "express";
 import { Database } from "./database";
+import { Temporal } from "@js-temporal/polyfill";
 
 // Refactor the following code to get rid of the legacy Date class.
 // Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
@@ -19,14 +20,15 @@ function createApp(database: Database) {
     const age = req.query.age ? parseInt(req.query.age as string) : undefined;
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
-    const date = parseDate(req.query.date as string);
+    const plainDate = parseDate(req.query.date as string);
+    const date = plainDate ? new Date(plainDate.toString()) : undefined;
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
-  function parseDate(dateString: string | undefined): Date | undefined {
+  function parseDate(dateString: string | undefined): Temporal.PlainDate | undefined {
     if (dateString) {
-      return new Date(dateString);
+      return Temporal.PlainDate.from(dateString);
     }
   }
 
