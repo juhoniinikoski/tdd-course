86e6d9f98d9a5334f5ff781e4c2a729220ae0137
Juho Niinikoski <niinikoskijuho@gmail.com>
2024-03-03 12:19:33 +0200

    created toString method

--- part2/src/Board.ts
+++ part2/src/Board.ts
@@ -1,13 +1,18 @@
+type Matrix = string[][];
+
 export class Board {
   width;
   height;
+  matrix: Matrix = [];
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
+
+    this.matrix = Array.from({ length: this.height }, () => ".".repeat(this.width).split(""));
   }
 
   toString() {
-    return "TODO";
+    return this.matrix.map((row) => row.join("").concat("\n")).join("");
   }
 }
