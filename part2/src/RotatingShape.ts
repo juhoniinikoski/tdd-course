81f2a10ed590b2dc2f4c8f47dc5c2f84132b2e24
Juho Niinikoski <niinikoskijuho@gmail.com>
2024-03-03 13:22:13 +0200

    added right rotation

--- part2/src/RotatingShape.ts
+++ part2/src/RotatingShape.ts
@@ -1,4 +1,6 @@
 export class RotatingShape {
+  width: number;
+  height: number;
   shape;
 
   static fromString(shape: string) {
@@ -13,6 +15,22 @@ export class RotatingShape {
 
   constructor(shape: string[][]) {
     this.shape = shape;
+    this.width = shape[0].length;
+    this.height = shape.length;
+  }
+
+  rotateRight() {
+    const size = this.shape.length;
+    for (let i = 0; i < size; i++) {
+      for (let j = i + 1; j < size; j++) {
+        [this.shape[i][j], this.shape[j][i]] = [this.shape[j][i], this.shape[i][j]];
+      }
+    }
+
+    for (let i = 0; i < size; i++) {
+      this.shape[i].reverse();
+    }
+    return new RotatingShape(this.shape);
   }
 
   toString() {
