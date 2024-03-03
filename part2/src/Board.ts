fb360249e0b36f24e2125637cb38806ca4bdbc05
Juho Niinikoski <niinikoskijuho@gmail.com>
2024-03-03 12:32:29 +0200

    added starting position to top center

--- part2/src/Board.ts
+++ part2/src/Board.ts
@@ -1,15 +1,23 @@
 type Matrix = string[][];
 
 export class Board {
-  width;
-  height;
+  width: number;
+  height: number;
   matrix: Matrix = [];
+  fallingBlock: string | undefined;
+  fallingBlockPosition: [number, number] | undefined;
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
 
     this.matrix = Array.from({ length: this.height }, () => ".".repeat(this.width).split(""));
+  }
+
+  drop(block: string) {
+    this.fallingBlock = block;
+    this.fallingBlockPosition = [0, Math.floor(this.width / 2)];
+    this.matrix[this.fallingBlockPosition[0]][this.fallingBlockPosition[1]] = block;
   }
 
   toString() {
