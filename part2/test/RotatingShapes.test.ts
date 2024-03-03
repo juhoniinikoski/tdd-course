de78d1db85a35ebdcc4816d162462f7a06928a8f
Juho Niinikoski <niinikoskijuho@gmail.com>
2024-03-03 13:43:25 +0200

    uncommented test cases

--- part2/test/RotatingShapes.test.ts
+++ part2/test/RotatingShapes.test.ts
@@ -25,7 +25,7 @@ describe("Rotating 3x3 shape", () => {
     );
   });
 
-  test.only("can be rotated left/counter-clockwise", () => {
+  test("can be rotated left/counter-clockwise", () => {
     expect(shape.rotateLeft().toString()).to.equalShape(
       `CFI
        BEH
@@ -34,42 +34,42 @@ describe("Rotating 3x3 shape", () => {
   });
 });
 
-// describe("Rotating 5x5 shape", () => {
-//   const shape = RotatingShape.fromString(
-//     `ABCDE
-//      FGHIJ
-//      KLMNO
-//      PQRST
-//      UVWXY`
-//   );
+describe("Rotating 5x5 shape", () => {
+  const shape = RotatingShape.fromString(
+    `ABCDE
+     FGHIJ
+     KLMNO
+     PQRST
+     UVWXY`
+  );
 
-//   test("initial orientation", () => {
-//     expect(shape.toString()).to.equalShape(
-//       `ABCDE
-//        FGHIJ
-//        KLMNO
-//        PQRST
-//        UVWXY`
-//     );
-//   });
+  test("initial orientation", () => {
+    expect(shape.toString()).to.equalShape(
+      `ABCDE
+       FGHIJ
+       KLMNO
+       PQRST
+       UVWXY`
+    );
+  });
 
-//   test.skip("can be rotated right/clockwise", () => {
-//     expect(shape.rotateRight().toString()).to.equalShape(
-//       `UPKFA
-//        VQLGB
-//        WRMHC
-//        XSNID
-//        YTOJE`
-//     );
-//   });
+  test("can be rotated right/clockwise", () => {
+    expect(shape.rotateRight().toString()).to.equalShape(
+      `UPKFA
+       VQLGB
+       WRMHC
+       XSNID
+       YTOJE`
+    );
+  });
 
-//   test.skip("can be rotated left/counter-clockwise", () => {
-//     expect(shape.rotateLeft().toString()).to.equalShape(
-//       `EJOTY
-//        DINSX
-//        CHMRW
-//        BGLQV
-//        AFKPU`
-//     );
-//   });
-// });
+  test("can be rotated left/counter-clockwise", () => {
+    expect(shape.rotateLeft().toString()).to.equalShape(
+      `EJOTY
+       DINSX
+       CHMRW
+       BGLQV
+       AFKPU`
+    );
+  });
+});
