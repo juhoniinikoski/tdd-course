17968d3731cbcd8ddb594e49cc803bfe0ee37c21
Juho Niinikoski <niinikoskijuho@gmail.com>
2024-04-22 20:57:46 +0300

    level 6 done

--- part2/test/RotatingFallingTetrominoes.test.ts
+++ part2/test/RotatingFallingTetrominoes.test.ts
@@ -2,6 +2,16 @@ import { beforeEach, describe, expect, test } from "vitest";
 import { Board } from "../src/Board";
 import { Tetromino } from "../src/Tetromino";
 
+const moveToBottom = (board: Board) => {
+  board.moveDown();
+  board.moveDown();
+  board.moveDown();
+  board.moveDown();
+  board.moveDown();
+  board.moveDown();
+  board.moveDown();
+};
+
 describe("Falling rotating tetrominoes", () => {
   let board: Board;
   beforeEach(() => {
@@ -78,83 +88,12 @@ describe("Falling rotating tetrominoes", () => {
     );
   });
 
-  // test("Cannot be rotated right when there is no room to rotate (I-shape) due to wall", () => {
-  //   board.drop(Tetromino.I_SHAPE);
-  //   board.rotateRight();
-  //   board.rotateRight();
-  //   board.rotateRight();
-  //   board.moveRight();
-  //   board.moveRight();
-  //   board.moveRight();
-  //   board.moveRight();
-  //   board.moveRight();
-  //   board.moveRight();
-  //   board.rotateRight();
-
-  //   expect(board.toString()).to.equalShape(
-  //     `..........
-  //      .........I
-  //      .........I
-  //      .........I
-  //      .........I
-  //      ..........`
-  //   );
-  // });
-
-  // test("Cannot be rotated right when there is no room to rotate (O-shape) due to wall", () => {
-  //   board.drop(Tetromino.O_SHAPE);
-  //   board.rotateRight();
-  //   board.rotateRight();
-  //   board.rotateRight();
-  //   board.moveRight();
-  //   board.moveRight();
-  //   board.moveRight();
-  //   board.moveRight();
-  //   board.moveRight();
-  //   board.moveRight();
-  //   board.rotateRight();
-
-  //   expect(board.toString()).to.equalShape(
-  //     `........OO
-  //      ........OO
-  //      ..........
-  //      ..........
-  //      ..........
-  //      ..........`
-  //   );
-  // });
-
-  // test("Cannot be rotated left when there is no room to rotate (T-shape) due to wall", () => {
-  //   board.drop(Tetromino.T_SHAPE);
-  //   board.rotateRight();
-  //   board.moveLeft();
-  //   board.moveLeft();
-  //   board.moveLeft();
-  //   board.moveLeft();
-  //   board.moveLeft();
-  //   board.rotateLeft();
-
-  //   expect(board.toString()).to.equalShape(
-  //     `T.........
-  //      TT........
-  //      T.........
-  //      ..........
-  //      ..........
-  //      ..........`
-  //   );
-  // });
-
   test("Cannot be rotated right when there is no room to rotate (T-shape) due to another element", () => {
     board.drop(Tetromino.O_SHAPE);
     board.moveRight();
     board.moveRight();
     board.moveRight();
-    board.moveDown();
-    board.moveDown();
-    board.moveDown();
-    board.moveDown();
-    board.moveDown();
-    board.moveDown();
+    moveToBottom(board);
 
     board.drop(Tetromino.T_SHAPE);
     board.tick();
@@ -183,12 +122,7 @@ describe("Falling rotating tetrominoes", () => {
     board.moveLeft();
     board.moveLeft();
     board.moveLeft();
-    board.moveDown();
-    board.moveDown();
-    board.moveDown();
-    board.moveDown();
-    board.moveDown();
-    board.moveDown();
+    moveToBottom(board);
 
     board.drop(Tetromino.T_SHAPE);
     board.tick();
@@ -250,6 +184,174 @@ describe("Falling rotating tetrominoes", () => {
        ..........
        ..........
        ..........`
+    );
+  });
+
+  test("Won't perform right wall kick if there is no room between element and wall", () => {
+    board.drop(Tetromino.O_SHAPE);
+    board.moveLeft();
+    board.moveLeft();
+    moveToBottom(board);
+
+    board.drop(Tetromino.T_SHAPE);
+    board.rotateLeft();
+    board.moveLeft();
+    board.moveLeft();
+    board.moveLeft();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.rotateRight();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       .T........
+       TTOO......
+       .TOO......`
+    );
+  });
+
+  test("Won't perform right wall kick if there is no room between element and wall", () => {
+    board.drop(Tetromino.O_SHAPE);
+    board.moveRight();
+    board.moveRight();
+    moveToBottom(board);
+
+    board.drop(Tetromino.T_SHAPE);
+    board.rotateRight();
+    board.moveRight();
+    board.moveRight();
+    board.moveRight();
+    board.moveRight();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.rotateRight();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ........T.
+       ......OOTT
+       ......OOT.`
+    );
+  });
+
+  test("Won't perform left wall kick if there is no room between element and wall", () => {
+    board.drop(Tetromino.O_SHAPE);
+    board.moveLeft();
+    board.moveLeft();
+    moveToBottom(board);
+
+    board.drop(Tetromino.T_SHAPE);
+    board.rotateLeft();
+    board.moveLeft();
+    board.moveLeft();
+    board.moveLeft();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.rotateLeft();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       .T........
+       TTOO......
+       .TOO......`
+    );
+  });
+
+  test("Won't perform left wall kick if there is no room between element and wall", () => {
+    board.drop(Tetromino.O_SHAPE);
+    board.moveRight();
+    board.moveRight();
+    moveToBottom(board);
+
+    board.drop(Tetromino.T_SHAPE);
+    board.rotateRight();
+    board.moveRight();
+    board.moveRight();
+    board.moveRight();
+    board.moveRight();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.rotateLeft();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ........T.
+       ......OOTT
+       ......OOT.`
+    );
+  });
+
+  test("Won't perform right wall kick if there is no room between two elements", () => {
+    board.drop(Tetromino.O_SHAPE);
+    board.moveRight();
+    board.moveRight();
+    moveToBottom(board);
+
+    board.drop(Tetromino.O_SHAPE);
+    board.moveLeft();
+    board.moveLeft();
+    moveToBottom(board);
+
+    board.drop(Tetromino.T_SHAPE);
+    board.rotateRight();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.rotateRight();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ....T.....
+       ..OOTTOO..
+       ..OOT.OO..`
+    );
+  });
+
+  test("Won't perform left wall kick if there is no room between two elements", () => {
+    board.drop(Tetromino.O_SHAPE);
+    board.moveRight();
+    board.moveRight();
+    moveToBottom(board);
+
+    board.drop(Tetromino.O_SHAPE);
+    board.moveLeft();
+    board.moveLeft();
+    moveToBottom(board);
+
+    board.drop(Tetromino.T_SHAPE);
+    board.rotateRight();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.moveDown();
+    board.rotateLeft();
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ....T.....
+       ..OOTTOO..
+       ..OOT.OO..`
     );
   });
 });
