7952186fc944e8dc5027131eb6da95bd4b453d2a
Juho Niinikoski <niinikoskijuho@gmail.com>
2024-04-28 11:31:15 +0300

    implemented remaining endpoints

---
+++ part5/backend/src/index.test.ts
@@ -0,0 +1,82 @@
+import { vi, describe, it, expect } from "vitest";
+import { app } from ".";
+import supertest from "supertest";
+import { createId } from "@paralleldrive/cuid2";
+
+const api = supertest(app);
+
+const mockTasks = [
+  { id: createId(), title: "task1", completed: false, archived: false },
+  { id: createId(), title: "task2", completed: false, archived: false },
+];
+
+const { mockCreate, mockRename, mockList, mockArchive } = vi.hoisted(() => {
+  return {
+    mockCreate: vi.fn(),
+    mockRename: vi.fn(),
+    mockList: vi.fn(),
+    mockArchive: vi.fn(),
+  };
+});
+
+vi.mock("./domain/task", () => {
+  return {
+    Task: {
+      create: mockCreate,
+      rename: mockRename,
+      list: mockList,
+      archive: mockArchive,
+    },
+  };
+});
+
+describe("get tasks", () => {
+  it("calls task domain function successfully and returns list of tasks with statuscode 200", async () => {
+    mockList.mockImplementationOnce(() => Promise.resolve(mockTasks[0]));
+    const res = await api.get("/tasks");
+    expect(mockList).toBeCalledTimes(1);
+    expect(res.status).toEqual(200);
+  });
+});
+
+describe("create task", () => {
+  it("calls task domain function successfully and returns the created task with statuscode 200", async () => {
+    mockCreate.mockImplementationOnce(() => Promise.resolve(mockTasks[0]));
+    const res = await api.post("/tasks").send({ title: "test-title" });
+    expect(mockCreate).toBeCalledTimes(1);
+    expect(res.status).toEqual(200);
+  });
+
+  it("returns 400 if user inputs invalid body", async () => {
+    const res = await api.post("/tasks").send({ test: "unknown-key" });
+    expect(res.status).toEqual(400);
+  });
+});
+
+describe("rename task", () => {
+  it("calls task domain function successfully and returns the renamed task with statuscode 200", async () => {
+    mockRename.mockImplementationOnce(() => Promise.resolve(mockTasks[0]));
+    const res = await api.put("/tasks/rename").send({ taskID: mockTasks[0].id, newTitle: "test-title" });
+    expect(mockRename).toBeCalledTimes(1);
+    expect(res.status).toEqual(200);
+  });
+
+  it("returns 400 if user inputs invalid body", async () => {
+    const res = await api.put("/tasks/rename").send({ test: "unknown-key" });
+    expect(res.status).toEqual(400);
+  });
+});
+
+describe("archive task", () => {
+  it("calls task domain function successfully and returns the archived task with statuscode 200", async () => {
+    mockArchive.mockImplementationOnce(() => Promise.resolve(mockTasks[0]));
+    const res = await api.put("/tasks/archive").send({ taskID: mockTasks[0].id });
+    expect(mockArchive).toBeCalledTimes(1);
+    expect(res.status).toEqual(200);
+  });
+
+  it("returns 400 if user inputs invalid body", async () => {
+    const res = await api.put("/tasks/archive").send({ test: "unknown-key" });
+    expect(res.status).toEqual(400);
+  });
+});
--- part5/backend/src/index.ts
+++ part5/backend/src/index.ts
@@ -1,11 +1,62 @@
 import express from "express";
+import { z } from "zod";
+import { Task } from "./domain/task";
 
 const PORT = 80;
 
-const app = express();
+export const app = express();
+app.use(express.json());
+
+const CreateTaskSchema = z.object({
+  title: z.string(),
+});
+
+const RenameTaskSchema = z.object({
+  taskID: z.string(),
+  newTitle: z.string(),
+});
+
+const ArchiveTaskSchema = z.object({
+  taskID: z.string(),
+});
 
 app.get("/", (req, res) => {
   res.send("Hello from backend");
+});
+
+app.get("/tasks", async (req, res) => {
+  const result = await Task.list();
+  return res.json(result);
+});
+
+app.post("/tasks", async (req, res) => {
+  try {
+    const { title } = CreateTaskSchema.parse(req.body);
+    const result = await Task.create(title);
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({ error: "Validation failed" });
+  }
+});
+
+app.put("/tasks/rename", async (req, res) => {
+  try {
+    const { taskID, newTitle } = RenameTaskSchema.parse(req.body);
+    const result = await Task.rename(taskID, newTitle);
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({ error: "Validation failed" });
+  }
+});
+
+app.put("/tasks/archive", async (req, res) => {
+  try {
+    const { taskID } = ArchiveTaskSchema.parse(req.body);
+    const result = await Task.archive(taskID);
+    res.json(result);
+  } catch (error) {
+    res.status(400).json({ error: "Validation failed" });
+  }
 });
 
 app.listen(PORT, () => {
